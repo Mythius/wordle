a5de2ea3c272be0b95541f8be223d0261b96428f
matthiassouthwick <southwickmatthias@gmail.com>
2024-03-17 16:21:52 -0600

    mylib boilerplate

---
+++ electron.js
@@ -0,0 +1,40 @@
+// add to package.json
+// "start": ".\\node_modules\\electron\\dist\\electron.exe ."
+// npx electron-builder
+
+const { app, BrowserWindow } = require('electron');
+
+const name = "";
+
+
+
+function createWindow () {
+  let win = new BrowserWindow({
+    width: 600,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: __dirname + '/site/icon.png'
+  })
+
+  win.loadFile('site/index.html')
+
+  win.setMenu(null);
+}
+
+app.on('ready', createWindow)
+
+/*
+
+  package.json
+
+  "build":{
+    "win": {
+      "icon": "site/icon.png"
+    }
+  }
+
+
+*/
